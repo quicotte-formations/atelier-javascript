5614a0323e6cff14d67520beb293650b07c76b03
quicotte-formation <quicotte@gmail.com>
2023-08-10 14:53:23 +0200

    v1.0

---
+++ Ressources/acteurs.js
@@ -0,0 +1,37 @@
+let personnes = [
+    {
+        'prenom': 'Lou',
+        'nom': 'Ferigno',
+        'age': 60
+    },
+    {
+        'prenom': 'Loustic',
+        'nom': 'Ferigno',
+        'age': 50
+    },
+    {
+        'prenom': 'Bruce',
+        'nom': 'Willis',
+        'age': 65
+    },
+    {
+        'prenom': 'Arnold',
+        'nom': 'Schwartzenegger',
+        'age': 60
+    },
+    {
+        'prenom': 'Sylverster',
+        'nom': 'Stallone',
+        'age': 67
+    },
+    {
+        'prenom': 'Renee',
+        'nom': 'Zelweger',
+        'age': 40
+    },
+    {
+        'prenom': 'Roschy',
+        'nom': 'Zem',
+        'age': 50
+    },
+]
